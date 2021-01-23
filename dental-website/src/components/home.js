e582efa9304bfd0cc5308f684250e5aaa972eb05
Paul Morales <paulginga@hotmail.com>
2021-01-23 11:13:43 -0600

    progress on the navbar with all nav btns

--- dental-website/src/components/home.js
+++ dental-website/src/components/home.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import './home.css';
+import logoImg from '../Images/GE_official_logo.png'
 
-
-function home(){
-return(
-    <div className='background'>
-
-    </div>
-)
+function home() {
+    return (
+        <div className='background'>
+            <div className='navBar'>
+                <div className='logo'>
+                    <img src={logoImg}/>
+                </div>
+                <div className='navLinks'>
+                    <ul>
+                        <a href="#"><li>HOME</li></a>
+                        <a href="#"><li>ABOUT</li></a>
+                        <a href="#"><li>SERVICES</li></a>
+                        <a href="#"><li className='scheduleBtn'>SCHEDULE APPOINTMENT</li></a>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default home;
