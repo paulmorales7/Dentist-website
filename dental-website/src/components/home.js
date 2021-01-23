1d6ccf98299fee89b8ab4746d35a33ee1e65b3f5
Paul Morales <paulginga@hotmail.com>
2021-01-23 16:26:43 -0600

    made home page responsive

--- dental-website/src/components/home.js
+++ dental-website/src/components/home.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import './home.css';
 import logoImg from '../Images/GE_official_logo.png'
+import { FaBars } from "react-icons/fa";
+import { GrFormClose } from "react-icons/gr";
+
+
+const nav = document.getElementsByClassName('navLinks');
+
+function showMenu(){
+    nav.style.right = "0";
+}
+
+function closeMenu(){
+    nav.style.right = "-200px"
+}
 
 function home() {
+ 
     return (
         <div className='background'>
             <div className='navBar'>
                 <div className='logo'>
-                    <img src={logoImg}/>
+                    <img src={logoImg} />
                 </div>
                 <div className='navLinks'>
+                    <GrFormClose className='closeIcon' onClick={closeMenu()}/>
                     <ul>
                         <a href="#"><li>HOME</li></a>
                         <a href="#"><li>ABOUT</li></a>
@@ -17,6 +32,11 @@ function home() {
                         <a href="#"><li className='scheduleBtn'>SCHEDULE APPOINTMENT</li></a>
                     </ul>
                 </div>
+                <FaBars className='barsIcon' onClick={showMenu()}/>
+            </div>
+            <div className='main-text'>
+                <h1>Smile about your Smile!</h1>
+                <button className='main-textBtn'>Book Online!</button>
             </div>
         </div>
     )
